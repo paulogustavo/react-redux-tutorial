a511c2278d8352817ec3db5085956f947986e7d2
Paulo Gustavo <pauloaguiar@seap.ma.gov.br>
2019-09-11 08:55:01 -0300

    separating actions in a different file

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import * as TodoActins from './store/actions/todos';
+import { bindActionCreators } from 'redux';
 
 const TodoList = ({todos, addTodo}) => {
     return (
@@ -24,8 +26,6 @@ const mapStateToProps = state => ({
     todos: state.todos
 });
  
-const mapDispatchToProps = dispatch => ({
-    addTodo: text => dispatch({type: 'ADD_TODO', payload:{ text: text}})
-});
+const mapDispatchToProps = dispatch => bindActionCreators(TodoActins, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
